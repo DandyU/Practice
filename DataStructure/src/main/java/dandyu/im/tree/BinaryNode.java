fe8466bf909b791d8c6dd5bf5dcb234e56a42590
Yuseon Hwang <niceyuseon@gmail.com>
2019-07-23 23:47:28 +0900

    Add tree

---
+++ DataStructure/src/main/java/dandyu/im/tree/BinaryNode.java
@@ -0,0 +1,9 @@
+package dandyu.im.tree;
+
+public class BinaryNode {
+
+    private int number;
+    private BinaryNode leftNode;
+    private BinaryNode rightNode;
+
+}
---
+++ DataStructure/src/main/java/dandyu/im/tree/BinaryTree.java
@@ -0,0 +1,16 @@
+package dandyu.im.tree;
+
+public class BinaryTree {
+
+    private BinaryNode rootNode;
+    private int size;
+
+    BinaryTree() {
+        rootNode = new BinaryNode();
+    }
+
+    public boolean add(int value) {
+        return false;
+    }
+
+}
